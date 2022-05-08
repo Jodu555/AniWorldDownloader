a11319d82fb7370da8517ee44b229ed0ec9fb945
Jodu555 <Jodu505@gmail.com>
2022-05-08 02:18:31 +0200

    generate the unique urls where the files should be downloaded

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 const seasons = 2, episodes = [26, 13];
 
 const start = 'https://aniworld.to/anime/stream/the-irregular-at-magic-high-school/';
+
+const urls = [];
+
+(async () => {
+
+    for (let i = 0; i < seasons; i++) {
+        const season = i + 1;
+        const episode = episodes[i];
+        for (let j = 1; j < episode + 1; j++) {
+            urls.push(start + `staffel-${season}/episode-${j}`)
+        }
+    }
+
+    console.log(urls);
+
+})();
