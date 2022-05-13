0590e9d157a0e8cbeb6abb39f2f67aaa5548d8c1
Jodu555 <Jodu505@gmail.com>
2022-05-13 11:58:00 +0200

    Manually added all the m3u8 links to the application in the future this hopefully inst necessayr

--- src/index.js
+++ src/index.js
@@ -22,15 +22,39 @@ const start = 'https://aniworld.to/anime/stream/the-irregular-at-magic-high-scho
 const urls = [];
 
 const m3Urls = [
-    'https://delivery-node-kassab.voe-network.net/hls/,6oarmxfuum33cszcr365fkr7wyxqk4lwisu3jl7tny7ucyk2wq5tg5zqvvxa,.urlset/master.m3u8',
-    'https://delivery-node-handal.voe-network.net/hls/,6oarnztao43lcszcrycmlpbaw3anaic6f7663enx52wugtt7sum6rldbyfcq,.urlset/master.m3u8'
+    'https://delivery-node-kassab.voe-network.net/hls/,6oarmxfuum33cszcr365fkr7wyxqk4lwisu3jl7tny7ucyk2wq5vqszqvvxa,.urlset/master.m3u8',
+    'https://delivery-node-handal.voe-network.net/hls/,6oarnztao43lcszcrycmlpbaw3anaic6f7663enx52wugtt7sumwrelbyfcq,.urlset/master.m3u8',
+    'https://delivery-node-rayyaa.voe-network.net/hls/,6oarmx5uum33cszcr365fmrlxj3xmgjluzxmzohdr6jyrsrna76ypmtmumwq,.urlset/master.m3u8',
+    'https://delivery-node-ishaq.voe-network.net/hls/,6oarmwnuum33cszcr365fl3sxx3bicgcab2wh6p5wozja5iuofn5tnlqi6na,.urlset/master.m3u8',
+    'https://delivery-node-shamoon.voe-network.net/hls/,6oarmxvuum33cszcr365fmtt5o6r6eg2dprvfxu7u6nqvcymfvsjedvkcsda,.urlset/master.m3u8',
+    'https://delivery-node-malak.voe-network.net/hls/,6oarmu5uum33cszcr365fm3twsjwlw525y5foqgrugn34lvzbqg6apkmaxaq,.urlset/master.m3u8',
+    'https://delivery-node-aladdin.voe-network.net/hls/,6oarmw5uum33cszcr365f3txv22ek5dwehwjrexxde5wfbjorvjmpg6aksza,.urlset/master.m3u8',
+    'https://delivery-node-jabalah.voe-network.net/hls/,6oarmvfuum33cszcr365f3z6xkm53w7l6wwruit2pmsxukc62che4e7z5ova,.urlset/master.m3u8',
+    'https://delivery-node-safar.voe-network.net/hls/,6oarmsfuum33cszcr365fljrxnni3ux6sh52lftt2n4to5o5lvgjgvnmj2mq,.urlset/master.m3u8',
+    'https://delivery-node-fawziyyah.voe-network.net/hls/,6oarn3cioy3lcszcr365fnjbwsh2liabwltdpmzbipzb7brwhc42mey5gkjq,.urlset/master.m3u8',
+    'https://delivery-node-layth.voe-network.net/hls/,6oarmufuum33cszcr365f2bm5exhmjkc3xqpozz2yfzsc7gl4ebljq5av3bq,.urlset/master.m3u8',
+    'https://delivery-node-dhakwan.voe-network.net/hls/,6oarmmfuum33cszcr365fnrf5vtxb4q4vfs67y5wajnvgfe2n3yisbpdvqsa,.urlset/master.m3u8',
+    'https://delivery-node-fiqar.voe-network.net/hls/,6oarmsnuum33cszcr365fpjv5l4wczsj267x6fqq6nxhuuh6dzq7ghd2alrq,.urlset/master.m3u8',
+    'https://delivery-node-thanaa.voe-network.net/hls/,6oarmlvuum33cszcr365fkjwvbfi7bo6j67yusu2atgfjr2w3n2p3n42u6sa,.urlset/master.m3u8',
+    'https://delivery-node-waqqas.voe-network.net/hls/,6oarmrfuum33cszcr365f3zxwroerbdqfwd2xwujglcwwt42uzrtaqpuwf5q,.urlset/master.m3u8',
+    'https://delivery-node-aram.voe-network.net/hls/,6oarmrnuum33cszcr365f23rx36pi4desf7dc74tvpsifzqrfo6my2l53xga,.urlset/master.m3u8',
+    'https://delivery-node-tawfiq.voe-network.net/hls/,6oarmmnuum33cszcr365fy3qvw676rcbyubwpet6hrcgtcfszsvq5lgemama,.urlset/master.m3u8',
+    'https://delivery-node-ruwayd.voe-network.net/hls/,6oarnpkioy3lcszcr365f3jnwc6iwlwb2jdrfnjvxsrqvy6egky6ybv6yqgq,.urlset/master.m3u8',
+    'https://delivery-node-sahlah.voe-network.net/hls/,6oarminuum33cszcr365fizpui2rzic7xcdmzysrhh256v7d4peemkabra6q,.urlset/master.m3u8',
+    'https://delivery-node-najjar.voe-network.net/hls/,6oarmnvuum33cszcr365fkbrvfd5ttsdy3rliemnjoaww7dopjl6ricx2aoq,.urlset/master.m3u8',
+    'https://delivery-node-hawra.voe-network.net/hls/,6oarn2cioy3lcszcr365flluvp4khdni5l3sflh6lpjmt47krrm3jshogs4q,.urlset/master.m3u8',
+    'https://delivery-node-sayegh.voe-network.net/hls/,6oarmonuum33cszcr365f2be4hpahwda5oakq7t7x6cfpygcngwbkfy5xcgq,.urlset/master.m3u8',
+    'https://delivery-node-harb.voe-network.net/hls/,6oarmlfuum33cszcr365f2drxz7y7mlj76stqztvdjgqek6j3k3y4gkswdla,.urlset/master.m3u8',
+    'https://delivery-node-essa.voe-network.net/hls/,6oarmkfuum33cszcr365fkrqwgysjxrv6o4qr252fdhiufkxta6h4ntcmcma,.urlset/master.m3u8',
+    'https://delivery-node-ubadah.voe-network.net/hls/,6oarmkvuum33cszcr365fmbxv3rdec7idtzqj2rvqgozpdkucyuoqgilqmfq,.urlset/master.m3u8',
+    'https://delivery-node-umniyah.voe-network.net/hls/,6oarmknuum33cszcr365f3jo5ieejmc7vlyqjj4yof2letuxuaqt6az3y64q,.urlset/master.m3u8',
 ]
 
 const wait = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
 
 (async () => {
     // const pathToExtension = `C:/Users/Jodu555/AppData/Local/Google/Chrome/User Data/Default/Extensions/cfhdojbkjhnklbpkdaibdccddilifddb/3.12_0`;
-    // console.log(fs.readdirSync(pathToExtension));
+    // console.log(a.readdirSync(pathToExtension));
 
     // return;
 
@@ -47,11 +71,10 @@ const wait = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
         }
     }
 
-    // console.log(urls);
-    // console.log(urls[0]);
-
     let i = 0;
     for (const m3url of m3Urls) {
+        console.log(`Started the download of ${urls[i].file}`);
+        console.log(`  Download: ${i + 1} / ${m3Urls.length + 1}`);
         await startDownloading(urls[i], m3url)
         i++;
     }
@@ -128,7 +151,6 @@ async function startBrowser(obj) {
 
 async function startDownloading(obj, m3u8URL) {
     const downloadPath = path.join(process.cwd(), 'Downloads', title, obj.folder.replace(' ', '-'));
-    console.log(obj, m3u8URL);
     fs.mkdirSync(downloadPath, { recursive: true });
     const m3u8ToMp4 = require("m3u8-to-mp4");
     const converter = new m3u8ToMp4();
