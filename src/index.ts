3b125c5fdf9f2b3eb917a38d1acf84dd926f6eae
Jodu555 <Jodu505@gmail.com>
2024-10-08 15:48:20 +0200

    removed the . to # replacement

--- src/index.ts
+++ src/index.ts
@@ -428,12 +428,12 @@ async function startDownloading(obj: ExtendedEpisodeDownload, m3u8URL: string) {
 	}
 
 	if (obj._animeFolder || obj._seriesFolder) {
-		downloadPath = path.join(downloadPath, obj._animeFolder || obj._seriesFolder, obj.folder.replace(' ', '-'));
+		downloadPath = path.join(downloadPath, obj._animeFolder || obj._seriesFolder, obj.folder);
 	} else {
-		downloadPath = path.join(downloadPath, title, obj.folder.replace(' ', '-'));
+		downloadPath = path.join(downloadPath, title, obj.folder);
 	}
 	fs.mkdirSync(downloadPath, { recursive: true });
-	await deepM3u8Conversion(m3u8URL, path.join(downloadPath, obj.file.replaceAll('.', '#') + '.mp4'));
+	await deepM3u8Conversion(m3u8URL, path.join(downloadPath, obj.file + '.mp4'));
 }
 
 async function deepM3u8Conversion(url: string, output: string) {
